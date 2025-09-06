9fd9d5ca20135bab74d62e536e5b7ba2a5d4a788
Linad43 <merzlikindaniil97@gmail.com>
2025-09-06 19:41:23 +0300

    add mod10 to main

--- MainProgram/MainProgram/main.cpp
+++ MainProgram/MainProgram/main.cpp
@@ -14,6 +14,8 @@ int main() {
 		std::cout << "6. Модуль 6\n";
 		std::cout << "7. Модуль 7\n";
 		std::cout << "8. Модуль 8\n";
+		std::cout << "9. Модуль 9\n";
+		std::cout << "10. Модуль 10\n";
 		std::cout << "\n0. Выход\n";
 
 		std::cin >> choise;
@@ -66,6 +68,18 @@ int main() {
 		case 8: {
 			system("cls");
 			modul8();
+			system("cls");
+			break;
+		};
+		case 9: {
+			system("cls");
+			modul9();
+			system("cls");
+			break;
+		};
+		case 10: {
+			system("cls");
+			modul10();
 			system("cls");
 			break;
 		};
