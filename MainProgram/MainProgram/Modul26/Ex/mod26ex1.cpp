08312a9b9d668f4cbad737f52dadc588cbcdccc7
Linad43 <merzlikindaniil97@gmail.com>
2025-12-22 18:20:41 +0300

    mod26 ex1 done

--- MainProgram/MainProgram/Modul26/Ex/mod26ex1.cpp
+++ MainProgram/MainProgram/Modul26/Ex/mod26ex1.cpp
@@ -1,8 +1,17 @@
 ﻿#include "..\header_modul26.h"
 
-enum status {
+enum Status {
 	PLAY, PAUSE, NEXT, STOP
 };
+
+std::tm createRandDate() {
+	time_t t;
+	std::tm tm;
+	time(&t);
+	tm = *localtime(&t);
+	tm.tm_year -= random(0, 40);
+	return tm;
+}
 
 class Track {
 
@@ -11,6 +20,11 @@ class Track {
 	int timeTrack;
 
 public:
+	Track(std::string nameTrack = "") {
+		name = nameTrack;
+		dateCreate = createRandDate();
+		timeTrack = random(50, 4 * 60);
+	}
 
 	std::string timeToString() {
 		int min = timeTrack / 60;
@@ -28,15 +42,26 @@ public:
 			+ "\ntime: " + timeToString()
 			+ "\n";
 	}
+	static std::tm createRandDate() {
+		time_t t;
+		std::tm tm;
+		time(&t);
+		tm = *localtime(&t);
+		tm.tm_year -= random(0, 40);
+		return tm;
+	}
+
 };
 
 class Player {
-public:
+//public:
 	std::map <std::string, Track> tracks;
 	std::map <std::string, Track>::iterator iter { tracks.begin() };
 
-//public:
-
+public:
+	void addTrack(std::string nameTrack) {
+		tracks[nameTrack] = Track(nameTrack);
+	}
 	void play() {
 		std::string nameTrack;
 		std::cout << "Input track name:\n";
@@ -52,12 +77,83 @@ public:
 		std::cout << "Track pause\n";
 	}
 	void next() {
-
+		for (int i = random(1, tracks.size()-1); i > 0; i--) {
+			if (iter == tracks.end()) {
+				iter = tracks.begin();
+			}
+			iter++;
+			if (iter == tracks.end()) {
+				iter = tracks.begin();
+			}
+		};
+		std::cout << "Play next track:\n";
+		std::cout << iter->second.toString();
+	}
+	void stop() {
+		std::cout << "Track stop\n";
 	}
 };
+
+
 
 void mod26ex1() {
 	std::cout << "1. Реализовать программу имитации аудиоплеера.\n";
 	Player player = Player();
-	auto iter = player.tracks.begin();
+	player.addTrack("one");
+	player.addTrack("two");
+	player.addTrack("three");
+	player.addTrack("four");
+	player.addTrack("five");
+	player.addTrack("six");
+	std::string command;
+	Status status = STOP;
+	Status changeStatus = STOP;
+
+	while (true)
+	{
+		std::cout << "Input command:\n";
+		std::cin >> command;
+		if (command == "play") {
+			changeStatus = PLAY;
+		}
+		else if (command == "pause") {
+			changeStatus = PAUSE;
+		}
+		else if (command == "next") {
+			changeStatus = NEXT;
+		}
+		else if (command == "stop") {
+			changeStatus = STOP;
+		}
+		else if (command == "exit") {
+			break;
+		}
+		switch (changeStatus)
+		{
+		case PLAY:
+			if (status != changeStatus) {
+				player.play();
+				status = changeStatus;
+			}
+			break;
+		case PAUSE:
+			if (status != changeStatus && status != STOP) {
+				player.pause();
+				status = changeStatus;
+			}
+			break;
+		case NEXT:
+			player.next();
+			status = PLAY;
+			break;
+		case STOP:
+			if (status != STOP) {
+				player.stop();
+				status = changeStatus;
+			}
+			break;
+		default:
+			break;
+		}
+	}
 }
