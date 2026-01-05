b5e2774557e0a7ae1b6583ea57c04b06e7474a60
Linad <linad43merzlikin@yandex.com>
2026-01-05 18:21:25 +0300

    modul 28 done

--- MainProgram/MainProgram/Modul28/Ex/mod28ex3.cpp
+++ MainProgram/MainProgram/Modul28/Ex/mod28ex3.cpp
@@ -60,14 +60,13 @@ public:
 	void addToOrders(Order order) {
 		orders.push_back(order);
 		mtxCout2.lock();
-		std::cout << "Create order\n";
+		std::cout << "Create order.\n";
 		std::cout << order.toString() << std::endl;
 		mtxCout2.unlock();
 		mtxKitchen.lock();
 		//std::thread cook(this, &cooking);
 		//threadCooking.emplace_back(&Kitchen::cooking);
 		cooking();
-		mtxKitchen.unlock();
 	}
 	void addReadyOrders(Order order) {
 		readyOrders.push_back(order);
@@ -79,25 +78,34 @@ public:
 		//Courier courier = Courier();
 		//threadDrivers.emplace_back(driveOrderMain, courier);
 		driveOrder();
-		mtxCourier.unlock();
 	}
 	void addFinishOrders(Order order) {
 		finishOrders.push_back(order);
 		mtxCout2.lock();
-		std::cout << "Order done\n";
+		std::cout << "Order done.\n";
 		std::cout << order.toString() << std::endl;
 		mtxCout2.unlock();
 	}
 	void cooking() {
 		Order order = *orders.begin();
 		orders.erase(orders.begin());
+		mtxCout2.lock();
+		std::cout << "Start cooking.\n";
+		std::cout << order.toString() << std::endl;
+		mtxCout2.unlock();
 		std::this_thread::sleep_for(std::chrono::seconds(random(5, 15)));
+		mtxKitchen.unlock();
 		addReadyOrders(order);
 	}
 	void driveOrder() {
 		Order order = *readyOrders.begin();
+		mtxCout2.lock();
+		std::cout << "Start drive order.\n";
+		std::cout << order.toString() << std::endl;
+		mtxCout2.unlock();
 		readyOrders.erase(readyOrders.begin());
 		std::this_thread::sleep_for(std::chrono::seconds(30));
+		mtxCourier.unlock();
 		addFinishOrders(order);
 	}
 };
@@ -119,9 +127,9 @@ void mod28ex3() {
 	Kitchen kitchen = Kitchen();
 	std::vector<std::thread> treadOrders;
 	for (int i = 0; i < countOrders; i++) {
-		std::this_thread::sleep_for(std::chrono::seconds(random(5, 10)));
 		Order order = Order();
 		treadOrders.emplace_back(addToOrdersMain, kitchen, order);
+		std::this_thread::sleep_for(std::chrono::seconds(random(5, 10)));
 	};
 	for (int i = 0; i < treadOrders.size(); i++) {
 		treadOrders[i].join();
