e261ac7c2ee5a666505eb9ca818267d6e2513e4a
Linad <linad43merzlikin@yandex.com>
2026-01-28 14:43:41 +0300

    mod33ex2.cpp done

--- MainProgram/MainProgram/src/Modul33/Ex/mod33ex2.cpp
+++ MainProgram/MainProgram/src/Modul33/Ex/mod33ex2.cpp
@@ -1,5 +1,53 @@
 ﻿#include "header_modul33.h"
 
+class myAccessException : public std::exception {
+    const char *what() const noexcept override {
+        return "Access fishing\n";
+    }
+};
+
 void mod33ex2() {
     std::cout << "2. Реализовать игру по ловле рыбы.\n";
+
+    std::string pond[] = {
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    };
+    pond[randomInt(0, 8)] = "fish";
+    int count = 0;
+    while (count < 3) {
+        int buf = randomInt(0, 8);
+        if (pond[buf].empty()) {
+            pond[randomInt(0, 8)] = "fake";
+            ++count;
+        }
+    }
+    count = 0;
+    while (true) {
+        std::cout << "Enter num sector (1-9):\n";
+        std::string input;
+        std::cin >> input;
+        if (!isDigit(input) || (stoi(input) < 0 && stoi(input) > 9)) {
+            throw std::invalid_argument("Invalid input number.");
+        }
+        ++count;
+        if (pond[stoi(input) - 1] == "fish") {
+            std::cout << "Used: " << count << " try\n";
+            throw myAccessException();
+            system("pause");
+        } else if (pond[stoi(input) - 1] == "fake") {
+            std::cout << "Used: " << count << " try\n";
+            throw std::runtime_error("You catch boot.\n");
+            system("pause");
+        } else {
+            std::cout << "Nothing, try again.\n";
+        }
+    }
 }
