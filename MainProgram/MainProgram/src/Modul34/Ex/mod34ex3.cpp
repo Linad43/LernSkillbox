14332dd22d3d52c30eb7ee88b29cfc0360a14f22
Linad <linad43merzlikin@yandex.com>
2026-01-29 22:48:32 +0300

    modal34ex3 done

--- MainProgram/MainProgram/src/Modul34/Ex/mod34ex3.cpp
+++ MainProgram/MainProgram/src/Modul34/Ex/mod34ex3.cpp
@@ -1,5 +1,25 @@
-﻿#include "header_modul34.h"
+﻿#include <filesystem>
+#include "header_modul34.h"
 
 void mod34ex3() {
     std::cout << "3. Задание 3.\n";
+    auto recursiveGetFileNamesByExtension =
+            [](const std::filesystem::path &path,
+               const std::string &extension) {
+        std::vector<std::filesystem::path> fileNames;
+        for (auto p: std::filesystem::recursive_directory_iterator(path)) {
+            std::cout << p.path() << "\n";
+            if (is_regular_file(p) && !p.path().extension().compare(extension)) {
+                fileNames.push_back(p);
+            }
+        }
+        return fileNames;
+    };
+    auto fileNames = recursiveGetFileNamesByExtension(
+        "C:\\Users\\Linad\\Downloads\\", ".torrent");
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    for (const auto &i: fileNames) std::cout << i << "\n";
 }
